0b93366930324ff4d0d61dde6be1af1e25f25798
Jingwen Owen Ou <jingweno@gmail.com>
2013-12-01 18:00:08 -0800

    Add sha1

--- gh.rb
+++ gh.rb
@@ -7,10 +7,16 @@ class Gh < Formula
          else
            "386"
          end
+  SHA1 = if MacOS.prefer_64_bit?
+           "f7f5fe32d9c96ed2b134bdb18671602255458874"
+         else
+           "0f2cd7d0408ef8398576b2fbb7504f8a21b8eac3"
+         end
 
   homepage "https://github.com/jingweno/gh"
   head "https://github.com/jingweno/gh.git"
   url "https://github.com/jingweno/gh/releases/download/v#{VERSION}/gh_#{VERSION}-snapshot_darwin_#{ARCH}.zip"
+  sha1 SHA1
   version VERSION
 
   def install
